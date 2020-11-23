b93bb201fafcb387b815ec81d31e014b945639bc
amitPorat <tehilamit@gmail.com>
2020-11-23 22:53:27 +0200

    Ordering the classes

--- src/classes/DWGraph_DS.java
+++ src/classes/DWGraph_DS.java
@@ -2,6 +2,7 @@ package classes;
 
 import api.directed_weighted_graph;
 import api.edge_data;
+import api.geo_location;
 import api.node_data;
 
 import java.util.Collection;
@@ -137,5 +138,103 @@ public class DWGraph_DS implements directed_weighted_graph {
     @Override
     public int getMC() {
         return 0;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class EdgeData implements edge_data{
+
+    /**
+     * The id of the source node of this edge.
+     *
+     * @return
+     */
+    @Override
+    public int getSrc() {
+        return 0;
+    }
+
+    /**
+     * The id of the destination node of this edge
+     *
+     * @return
+     */
+    @Override
+    public int getDest() {
+        return 0;
+    }
+
+    /**
+     * @return the weight of this edge (positive value).
+     */
+    @Override
+    public double getWeight() {
+        return 0;
+    }
+
+    /**
+     * Returns the remark (meta data) associated with this edge.
+     *
+     * @return
+     */
+    @Override
+    public String getInfo() {
+        return null;
+    }
+
+    /**
+     * Allows changing the remark (meta data) associated with this edge.
+     *
+     * @param s
+     */
+    @Override
+    public void setInfo(String s) {
+
+    }
+
+    /**
+     * Temporal data (aka color: e,g, white, gray, black)
+     * which can be used be algorithms
+     *
+     * @return
+     */
+    @Override
+    public int getTag() {
+        return 0;
+    }
+
+    /**
+     * This method allows setting the "tag" value for temporal marking an edge - common
+     * practice for marking by algorithms.
+     *
+     * @param t - the new value of the tag
+     */
+    @Override
+    public void setTag(int t) {
+
     }
 }
